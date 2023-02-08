6c8fb9cbaad3e9fcdfcbdbf868173d44d74ca24f
Matt Kazmar <mkazmar@aviatrix.com>
2023-02-08 15:25:42 -0600

    fix tf variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,11 @@
 # Environment
 variable "project" { type = string }
-variable "region" { type = string }
-
+variable "zone" { type = string }
 variable "aviatrix_controller_admin_email" { type = string }
 variable "aviatrix_controller_admin_password" { type = string }
 variable "aviatrix_customer_id" { type = string }
 variable "controller_name" { type = string }
-variable "incoming_ssl_cidrs" { type = list(any) }
+variable "incoming_ssl_cidrs" { type = string }
 variable "image" { type = string }
 
 locals {
