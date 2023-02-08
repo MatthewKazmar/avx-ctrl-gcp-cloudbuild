ccb246d3a2ed7ce6fd097d14f3c08b1921a722ed
Matt Kazmar <mkazmar@aviatrix.com>
2023-02-08 16:02:17 -0600

    Need cloud builder ip in acl

--- variables.tf
+++ variables.tf
@@ -7,7 +7,8 @@ variable "aviatrix_customer_id" { type = string }
 variable "controller_name" { type = string }
 variable "incoming_ssl_cidrs" { type = string }
 variable "image" { type = string }
+variable "myip" { type = string}
 
 locals {
-  incoming_ssl_cidrs = split(",", var.incoming_ssl_cidrs)
+  incoming_ssl_cidrs = concat(split(",", var.incoming_ssl_cidrs), "${var.myip}/32")
 }
