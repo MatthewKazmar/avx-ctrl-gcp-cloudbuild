b8eeb27bca566d488c5f3f11480d526d0080f2cb
Matt Kazmar <mkazmar@aviatrix.com>
2023-02-08 15:05:16 -0600

    fix yaml formating (maybe)

--- apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "[APPLY] Installing Python3 and requirements."
-apk add --no-cache python3 py3-pip
-pip install -r .terraform/modules/aviatrix-controller-gcp/requirements.txt
-
-echo "[APPLY] Running Terraform apply."
-terraform apply
