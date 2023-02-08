933bcee37dc063ba605d7a071e3acc8cc0b5b589
Matt Kazmar <mkazmar@aviatrix.com>
2023-02-08 14:37:37 -0600

    first

--- main.tf
+++ main.tf
@@ -8,18 +8,30 @@ terraform {
 
 provider "google" {
   project     = var.project
-  region      = var.region
   zone        = var.zone
-  credentials = var.gcloud_project_credentials_filepath
+}
+
+# We need a json file for a service account in the workspace.
+data  "google_service_account" "aviatrix" {
+  account_id = var.gcp_account_id
+}
+
+resource "google_service_account_key" "aviatrix" {
+  service_account_id = data.google_service_account.aviatrix.aviatrix.name
+}
+
+resource "local_sensitive_file" "gcp_json" {
+  filename = "gcp.json"
+  content  = google_service_account_key.aviatrix.private_key
 }
 
 module "aviatrix-controller-gcp" {
   source                              = "AviatrixSystems/gcp-controller/aviatrix"
-  access_account_name                 = var.access_account_name
+  access_account_name                 = var.project
   aviatrix_controller_admin_email     = var.aviatrix_controller_admin_email
   aviatrix_controller_admin_password  = var.aviatrix_controller_admin_password
   aviatrix_customer_id                = var.aviatrix_customer_id
-  gcloud_project_credentials_filepath = var.gcloud_project_credentials_filepath
+  gcloud_project_credentials_filepath = resource.local_sensitive_file.gcp_json.filename
   incoming_ssl_cidrs                  = var.incoming_ssl_cidrs
   image                               = var.image
 }
