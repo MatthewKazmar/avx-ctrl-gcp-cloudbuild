4475d2fc82f8d85e94c99be49b1c85cb9f4bdf9c
Matt Kazmar <mkazmar@aviatrix.com>
2023-02-17 10:13:00 -0600

    more script

--- run-terraform.sh
+++ run-terraform.sh
@@ -4,7 +4,7 @@ STAGE="[$1]:"
 BUCKET="bucket=$TF_VAR_state_bucket"
 PREFIX="prefix=avx/$1"
 
-echo "${STAGE} Running Terraform Init"
-terraform -chdir=$1 -compact-warnings -backend-config=BUCKET -backend-config=PREFIX
-echo "${STAGE} Running Terraform Apply."
+echo "$STAGE Running Terraform Init"
+terraform -chdir=$1 -compact-warnings -backend-config=$BUCKET -backend-config=$PREFIX
+echo "$STAGE Running Terraform Apply."
 terraform -chdir=$1 -compact-warnings -auto-approve -lock=false
