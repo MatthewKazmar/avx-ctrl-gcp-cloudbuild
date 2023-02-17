4f723bd0c9dd0ca0d02ff6fc26630ce577799d86
Matt Kazmar <mkazmar@aviatrix.com>
2023-02-16 22:46:09 -0600

    index

--- controller-network/outputs.tf
+++ controller-network/outputs.tf
@@ -1,3 +1,3 @@
 output "subnetwork_self_link" {
-  value = local.create_vpc == 0 ? var.subnetwork_self_link : google_compute_subnetwork.avx_subnetwork.self_link
+  value = local.create_vpc == 0 ? var.subnetwork_self_link : google_compute_subnetwork.avx_subnetwork[0].self_link
 }
